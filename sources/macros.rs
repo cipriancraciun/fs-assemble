56cfe36c97ec5e57d5b04e5d59c6a39f913fbc74
Ciprian Dorin Craciun <ciprian@volution.ro>
2020-11-02 01:53:13 +0200

    [sources][tools]  Minor fixups and enhancements.

--- sources/macros.rs
+++ sources/macros.rs
@@ -6,8 +6,8 @@ macro_rules! fail {
 	( $_code : expr, $_message : expr ) => {
 		return Err ($crate::tools::error ($_code, format_args! ($_message)));
 	};
-	( $_code : expr, $_format : expr, $_arguments : expr ) => {
-		return Err ($crate::tools::error ($_code, format_args! ($_format, $_arguments)));
+	( $_code : expr, $_format : expr, $( $_arguments : expr ),* ) => {
+		return Err ($crate::tools::error ($_code, format_args! ($_format, $( $_arguments ),* )));
 	};
 }
 
@@ -15,23 +15,27 @@ macro_rules! fail {
 
 
 macro_rules! log_define {
-	( $_name : ident, $_slug : literal ) => {
+	( $_name : ident, $_slug : literal, $_level : literal ) => {
 		
 		#[ allow (unused_macros) ]
 		macro_rules! $_name {
 			( $_code : expr, $_message : expr ) => {
-				$crate::tools::log ($_slug, $_code, format_args! ($_message));
+				$crate::tools::log ($_slug, $_level, $_code, format_args! ($_message));
 			};
-			( $_code : expr, $_format : expr, $_arguments : tt ) => {
-				$crate::tools::log ($_slug, $_code, format_args! ($_format, $_arguments));
+			( $_code : expr, $_format : expr, $_argument_1 : expr ) => {
+				$crate::tools::log ($_slug, $_level, $_code, format_args! ($_format, $_argument_1));
+			};
+			( $_code : expr, $_format : expr, $_argument_1 : expr, $_argument_2 : expr ) => {
+				$crate::tools::log ($_slug, $_level, $_code, format_args! ($_format, $_argument_1, $_argument_2));
 			};
 		}
 	}
 }
 
-log_define! (log_error, "[ee]");
-log_define! (log_warning, "[ww]");
-log_define! (log_notice, "[ii]");
-log_define! (log_information, "[ii]");
-log_define! (log_debug, "[dd]");
+log_define! (log_error, "[ee]", 60000);
+log_define! (log_warning, "[ww]", 50000);
+log_define! (log_notice, "[ii]", 40000);
+log_define! (log_information, "[ii]", 30000);
+log_define! (log_debug, "[dd]", 20000);
+log_define! (log_trace, "[dd]", 10000);
 
--- sources/tools.rs
+++ sources/tools.rs
@@ -1,28 +1,35 @@
 
 
-use ::std::fmt;
-use ::std::io;
+use crate::prelude::*;
 
 
 
 
-pub fn log (_slug : &str, _code : u32, _message : impl fmt::Display) -> () {
+pub type Outcome<Value> = Result<Value, io::Error>;
+
+
+
+
+pub fn log (_slug : &str, _level : u16, _code : u32, _message : impl fmt::Display) -> () {
+	if _level < 20000 {
+		return;
+	}
 	match (_slug, _code) {
 		("", 0) =>
 			eprintln! ("{}", _message),
 		("", _) =>
-			eprintln! ("[{:8x}]  {}", _code, _message),
+			eprintln! ("[{:08x}]  {}", _code, _message),
 		(_, 0) =>
 			eprintln! ("{} {}", _slug, _message),
 		(_, _) =>
-			eprintln! ("{} [{:8x}]  {}", _slug, _code, _message),
+			eprintln! ("{} [{:08x}]  {}", _slug, _code, _message),
 	}
 }
 
 
 pub fn error (_code : u32, _message : impl fmt::Display) -> io::Error {
 	
-	let _message = format! ("[{:8x}]  {}", _code, _message);
+	let _message = format! ("[{:08x}]  {}", _code, _message);
 	
 	io::Error::new (io::ErrorKind::Other, _message)
 }
