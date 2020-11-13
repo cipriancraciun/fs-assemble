bda4f5917abd258386ab921302b93364cd99b32b
Ciprian Dorin Craciun <ciprian@volution.ro>
2020-11-13 23:58:31 +0200

    [sources][prelude]  Minor updates.

--- sources/prelude.rs
+++ sources/prelude.rs
@@ -1,39 +1,44 @@
 
 
-pub use ::std::env;
-pub use ::std::fmt;
-pub use ::std::ffi;
-pub use ::std::fs;
-pub use ::std::io;
-pub use ::std::mem;
-pub use ::std::ops;
-pub use ::std::path;
-pub use ::std::process;
-
-pub use ::globset;
-pub use ::regex::bytes as regexb;
-pub use ::walkdir;
+#![ allow (unused_imports) ]
 
 
-pub use ::std::ffi::OsStr;
-pub use ::std::ffi::OsString;
-pub use ::std::os::unix::ffi::OsStrExt;
-pub use ::std::os::unix::ffi::OsStringExt;
+pub(crate) use ::std::env;
+pub(crate) use ::std::fmt;
+pub(crate) use ::std::ffi;
+pub(crate) use ::std::fs;
+pub(crate) use ::std::io;
+pub(crate) use ::std::mem;
+pub(crate) use ::std::ops;
+pub(crate) use ::std::path;
+pub(crate) use ::std::process;
 
-pub use ::std::path::Path;
-pub use ::std::path::PathBuf;
+pub(crate) use ::globset;
+pub(crate) use ::regex::bytes as regexb;
+pub(crate) use ::walkdir;
+pub(crate) use ::pathdiff as path_diff;
+pub(crate) use ::path_clean as path_clean;
 
-pub use ::std::ops::Bound;
 
-pub use ::std::collections::hash_map;
-pub use ::std::collections::hash_map::HashMap;
-pub use ::std::collections::hash_set;
-pub use ::std::collections::hash_set::HashSet;
+pub(crate) use ::std::ffi::OsStr;
+pub(crate) use ::std::ffi::OsString;
+pub(crate) use ::std::os::unix::ffi::OsStrExt;
+pub(crate) use ::std::os::unix::ffi::OsStringExt;
 
-pub use ::std::collections::btree_map;
-pub use ::std::collections::btree_map::BTreeMap;
-pub use ::std::collections::btree_set;
-pub use ::std::collections::btree_set::BTreeSet;
+pub(crate) use ::std::path::Path;
+pub(crate) use ::std::path::PathBuf;
+
+pub(crate) use ::std::ops::Bound;
+
+pub(crate) use ::std::collections::hash_map;
+pub(crate) use ::std::collections::hash_map::HashMap;
+pub(crate) use ::std::collections::hash_set;
+pub(crate) use ::std::collections::hash_set::HashSet;
+
+pub(crate) use ::std::collections::btree_map;
+pub(crate) use ::std::collections::btree_map::BTreeMap;
+pub(crate) use ::std::collections::btree_set;
+pub(crate) use ::std::collections::btree_set::BTreeSet;
 
 
 
@@ -49,5 +54,5 @@ pub mod fsas {
 }
 
 
-pub use self::fsas::Outcome;
+pub(crate) use self::fsas::Outcome;
 
